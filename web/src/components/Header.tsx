8acfd99c3dbef45f05b16bd961dbd4a8247f0f6d
LucasSousa09 <lucasrodrigues.sousa09@gmail.com>
2022-10-14 15:51:55 -0300

    Header component media query classes organized

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -21,7 +21,7 @@ export function Header(){
        </button>
        {
         navHidden ? (
-          <nav className={`flex sm:flex-col md:flex-row sm:items-end sm:pt-6 sm:px-4 md:p-0 sm:gap-6 sm:hidden md:block sm:absolute sm:top-0 sm:right-0 sm:bottom-0 md:static sm:h-[100vh] md:h-auto sm:w-[200px] md:w-auto sm:bg-zinc-700 md:bg-zinc-800 z-50`}>
+          <nav className={`flex sm:absolute sm:top-0 sm:right-0 sm:bottom-0 sm:h-[100vh] sm:w-[200px] sm:flex-col sm:items-end sm:pt-6 sm:px-4 sm:bg-zinc-700 sm:hidden md:flex-row md:p-0 sm:gap-6 md:block md:static  md:h-auto md:w-auto md:bg-zinc-800 z-50`}>
             {
               navHidden !== 'hidden' && (
               <button onClick={handleClick}> 
@@ -29,14 +29,14 @@ export function Header(){
               </button>
               )
             }
-            <ul className="flex sm:flex-col md:flex-row sm:gap-6 md:gap-0 sm:w-full md:w-[300px] md:text-sm lg:text-base justify-between font-medium text-zinc-400">
-              <li className="hover:text-zinc-50 sm:border-b-2 md:border-0 sm:border-zinc-600 sm:w-full sm:text-end"> <a href="#about-me"> About Me </a> </li>
-              <li className="hover:text-zinc-50 sm:border-b-2 md:border-0 sm:border-zinc-600 sm:w-full sm:text-end"> <a href="#my-works"> My work's </a> </li>
-              <li className="hover:text-zinc-50 sm:border-b-2 md:border-0 sm:border-zinc-600 sm:w-full sm:text-end"> <a href="#"> Hire me </a> </li>
+            <ul className="flex justify-between font-medium text-zinc-400 sm:flex-col sm:gap-6 sm:w-full md:flex-row md:gap-0 md:w-[300px] md:text-sm lg:text-base">
+              <li className="hover:text-zinc-50 sm:border-b-2 sm:border-zinc-600 sm:w-full sm:text-end md:border-0"> <a href="#about-me"> About Me </a> </li>
+              <li className="hover:text-zinc-50 sm:border-b-2 sm:border-zinc-600 sm:w-full sm:text-end md:border-0"> <a href="#my-works"> My work's </a> </li>
+              <li className="hover:text-zinc-50 sm:border-b-2 sm:border-zinc-600 sm:w-full sm:text-end md:border-0"> <a href="#"> Hire me </a> </li>
             </ul>
           </nav>
         ) : (
-          <nav className={`sm:flex-col md:flex-row sm:items-end sm:pt-6 sm:px-4 md:p-0 sm:gap-6 sm:flex md:flex sm:absolute sm:top-0 sm:right-0 sm:bottom-0 md:static sm:h-[100vh] md:h-auto sm:w-[200px] md:w-auto sm:bg-zinc-700 md:bg-zinc-800 z-50`}>
+          <nav className={`z-50 flex sm:absolute sm:top-0 sm:right-0 sm:bottom-0 sm:flex-col sm:items-end sm:pt-6 sm:px-4 sm:gap-6 sm:h-[100vh] sm:w-[200px] sm:bg-zinc-700 md:p-0 md:static md:flex-row md:h-auto md:w-auto md:bg-zinc-800`}>
             {
               navHidden !== 'hidden' && (
               <button onClick={handleClick}> 
@@ -44,10 +44,10 @@ export function Header(){
               </button>
               )
             }
-            <ul className="flex sm:flex-col md:flex-row sm:gap-6 md:gap-0 sm:w-full md:w-[300px] md:text-sm lg:text-base justify-between font-medium text-zinc-400">
-              <li className="hover:text-zinc-50 sm:border-b-2 md:border-0 sm:border-zinc-600 sm:w-full sm:text-end"> <a href="#about-me"> About Me </a> </li>
-              <li className="hover:text-zinc-50 sm:border-b-2 md:border-0 sm:border-zinc-600 sm:w-full sm:text-end"> <a href="#my-works"> My work's </a> </li>
-              <li className="hover:text-zinc-50 sm:border-b-2 md:border-0 sm:border-zinc-600 sm:w-full sm:text-end"> <a href="#"> Hire me </a> </li>
+            <ul className="flex sm:flex-col sm:gap-6 sm:w-full md:flex-row md:gap-0 md:w-[300px] md:text-sm lg:text-base justify-between font-medium text-zinc-400">
+              <li className="hover:text-zinc-50 sm:border-b-2 sm:border-zinc-600 sm:w-full sm:text-end md:border-0"> <a href="#about-me"> About Me </a> </li>
+              <li className="hover:text-zinc-50 sm:border-b-2 sm:border-zinc-600 sm:w-full sm:text-end md:border-0"> <a href="#my-works"> My work's </a> </li>
+              <li className="hover:text-zinc-50 sm:border-b-2 sm:border-zinc-600 sm:w-full sm:text-end md:border-0"> <a href="#"> Hire me </a> </li>
             </ul>
           </nav>
         )
