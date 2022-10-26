a7b2c1f432592af0810274bd32425c5425c0e574
LucasSousa09 <lucasrodrigues.sousa09@gmail.com>
2022-10-26 12:57:06 -0300

    highlight navigation

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,8 +7,11 @@ import { MyWorks } from './pages/MyWorks'
 import { AboutMe } from "./pages/AboutMe";
 import { Home } from "./pages/Home";
 
+type CurrentPageProps = "home" | "aboutMe" | "myWorks" | "hireMe"
+
+
 export function App() {
-  const [currentOffsetTop, setCurrentOffsetTop] = useState(0)
+  const [currentPageNav, setCurrentPageNav] = useState<CurrentPageProps>("home")
   
   const container = useRef<HTMLDivElement>(null)
   const homeContainer = useRef<HTMLDivElement>(null)
@@ -17,12 +20,31 @@ export function App() {
   const hireMeContainerRef = useRef<HTMLDivElement>(null)
 
   function handleOnScroll() {
-    console.log(aboutMeContainerRef.current?.offsetTop)
+    if(container.current !== null || aboutMeContainerRef.current !== null || homeContainer.current || myWorksContainerRef.current || hireMeContainerRef.current ){
+      const containerScrollTop = container.current?.scrollTop || 0
+      const homeTop = 0;
+      const aboutMeTop = aboutMeContainerRef.current?.offsetTop || 0
+      const myWorksTop = myWorksContainerRef.current?.offsetTop || 0
+      const hireMeTop = hireMeContainerRef.current?.offsetTop   || 0
+
+      if(containerScrollTop < aboutMeTop-93){
+        setCurrentPageNav("home")
+      }
+      else if(containerScrollTop >= aboutMeTop-93 && containerScrollTop < myWorksTop-93){
+        setCurrentPageNav("aboutMe")
+      }
+      else if(containerScrollTop >= myWorksTop-93 && containerScrollTop < hireMeTop-93){
+        setCurrentPageNav("myWorks")
+      }
+      else if(containerScrollTop >= hireMeTop-100){
+        setCurrentPageNav("hireMe")
+      }
+    }
   }
 
   return (
     <div className="sm:mt-[88px] md:mt-[92px] sm:h-[calc(100vh-88px)] md:h-[calc(100vh-92px)] overflow-y-scroll scroll-smooth" ref={container} onScroll={handleOnScroll}>
-      <Header />
+      <Header currentPageNav={currentPageNav} />
       <Home homeContainer={homeContainer} />
       <AboutMe aboutMeContainerRef={aboutMeContainerRef}/>
       <MyWorks myWorksContainerRef={myWorksContainerRef} />
--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,7 +1,13 @@
 import { List, XCircle } from 'phosphor-react' 
 import { useState } from 'react'
 
-export function Header(){
+import '../styles/highlightlink.css'
+
+interface HeaderProps {
+  currentPageNav: string
+}
+
+export function Header({ currentPageNav }: HeaderProps){
   const [navHidden, setNavHidden] = useState('hidden')
 
   function handleClick(){
@@ -30,10 +36,10 @@ export function Header(){
               )
             }
             <ul className="flex justify-between font-medium text-zinc-400 sm:flex-col sm:gap-6 sm:w-full md:flex-row md:gap-0 md:w-[400px] md:text-sm lg:w-[450px] lg:text-base">
-              <li className="hover:text-zinc-50 sm:border-b-2 sm:border-zinc-600 sm:w-full sm:text-end md:border-0"> <a href="#home"> Home </a> </li>
-              <li className="hover:text-zinc-50 sm:border-b-2 sm:border-zinc-600 sm:w-full sm:text-end md:border-0"> <a href="#about-me"> About Me </a> </li>
-              <li className="hover:text-zinc-50 sm:border-b-2 sm:border-zinc-600 sm:w-full sm:text-end md:border-0"> <a href="#my-works"> My work's </a> </li>
-              <li className="hover:text-zinc-50 sm:border-b-2 sm:border-zinc-600 sm:w-full sm:text-end md:border-0"> <a href="#hire-me"> Hire me </a> </li>
+              <li className={`${currentPageNav === "home" ? 'highlight' : ''} active:text-zinc-300 hover:text-zinc-50 sm:border-b-2 sm:border-zinc-600 sm:w-full sm:text-end md:border-0`}> <a href="#home"> Home </a> </li>
+              <li className={`${currentPageNav === "aboutMe" ? 'highlight' : ''} active:text-zinc-300 hover:text-zinc-50 sm:border-b-2 sm:border-zinc-600 sm:w-full sm:text-end md:border-0`}> <a href="#about-me"> About Me </a> </li>
+              <li className={`${currentPageNav === "myWorks" ? 'highlight' : ''} active:text-zinc-300 hover:text-zinc-50 sm:border-b-2 sm:border-zinc-600 sm:w-full sm:text-end md:border-0`}> <a href="#my-works"> My work's </a> </li>
+              <li className={`${currentPageNav === "hireMe" ? 'highlight' : ''} active:text-zinc-300 hover:text-zinc-50 sm:border-b-2 sm:border-zinc-600 sm:w-full sm:text-end md:border-0`}> <a href="#hire-me"> Hire me </a> </li>
             </ul>
           </nav>
         ) : (
