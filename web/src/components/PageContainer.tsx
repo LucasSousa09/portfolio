8b71428407ae4c4539e421bb1d26bcb950d8ccd1
LucasSousa09 <lucasrodrigues.sousa09@gmail.com>
2022-10-27 11:59:28 -0300

    Some layout fixes

--- web/src/components/PageContainer.tsx
+++ web/src/components/PageContainer.tsx
@@ -8,7 +8,7 @@ interface PageContainerProps{
 
 export function PageContainer({ children, id, pageContainerRef }: PageContainerProps){
   return (
-    <div id={id} className="flex flex-col sm:pt-[22px] sm:min-h-[calc(100vh-88px)] sm:mx-8 md:pt-0 md:h-[calc(100vh-92px)] md:mx-12 lg:mx-14 xl:mx-auto xl:max-w-[1148px]" ref={pageContainerRef}>
+    <div id={id} className="flex flex-col sm:pt-[22px] sm:min-h-[calc(100vh-88px)] sm:mx-8 md:pt-0 md:min-h-[600px] md:h-[calc(100ch-92px)] md:mx-12 lg:mx-14 xl:mx-auto xl:max-w-[1148px]" ref={pageContainerRef}>
       {children}
     </div>
   )
